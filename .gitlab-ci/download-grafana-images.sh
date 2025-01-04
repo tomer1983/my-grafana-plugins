66b118416d102279892dc42d8184e75d8d7c9704
Tomer Aharoni <tomer1983@gmail.com>
2025-01-04 20:39:26 +0000

    Update file download-grafana-images.sh

--- .gitlab-ci/download-grafana-images.sh
+++ .gitlab-ci/download-grafana-images.sh
@@ -6,7 +6,6 @@ GRAFANA_API_URL="https://grafana.com/api/plugins"
 PLUGINS_FILE="$1"
 PLUGINS_DIR="grafana-plugins"
 
-echo "
 
 git config --global user.email "tomer1983@gmail.com"
 git config --global user.name "tomer1983"
